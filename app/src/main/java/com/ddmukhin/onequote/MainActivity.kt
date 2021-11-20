d7c31e3214bb45760d811ab387bf6b7213fa9dfc
ddmukhin <dmitrij.muhin@stroyland.ru>
2021-11-20 23:44:01 +0300

    initial commit

---
+++ app/src/main/java/com/ddmukhin/onequote/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ddmukhin.onequote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.paperdb.Paper
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
